34fad2b03c10f870b7deb51c17aae24c257ee954
DavidM444 <david541gamer@gmail.com>
2024-01-27 19:56:29 -0500

    practica 1

--- src/main/java/com/programandoenjava/parte1/Parte1.java
+++ src/main/java/com/programandoenjava/parte1/Parte1.java
@@ -1,5 +1,8 @@
 package com.programandoenjava.parte1;
 
+import java.util.Arrays;
+import java.util.stream.*;
+
 public class Parte1 {
 
     /**
@@ -9,7 +12,35 @@ public class Parte1 {
      * @param cadena cadena de entrada
      * @return cadena invertida
      */
-    public String invertirPalabras(final String cadena) {;
-        return null;
+    public String invertirPalabras(final String cadena) {
+
+//StringBuilder crea objetos que representa secuncia mutable de caracteres, es decir que un objeto que puede ser modificado.
+        //A diferencia de String que no crea una secuecia mutable. Cundo concatenamos un string se crea otro ovjeto nuevo con esa concatenacion.
+
+        StringBuilder res= new StringBuilder();
+
+        int posicion = 0;
+
+        if(cadena==null) {return "";};
+        char[] arrayChart = cadena.toCharArray();
+        //System.out.println("let "+arrayChart.toString());
+
+
+        for (char letter : arrayChart) {
+            System.out.println("letras "+Arrays.toString(arrayChart)+ " letra :"+letter);
+            System.out.println("psocion "+posicion);
+
+            if(!Character.isLetter(letter)) {
+                res.append(letter);
+                System.out.println("carac "+letter + " res: "+res );
+                posicion = res.lastIndexOf(Character.toString(letter)) + 1;
+                continue;
+            }
+
+            res.insert(posicion, letter);
+
+        }
+        return res.toString();
+
     }
 }
